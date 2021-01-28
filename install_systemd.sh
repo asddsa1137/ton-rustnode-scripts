780ed84fc61ba7ab4eb978bc4b272b86d9bed6fe
Lev <arryalev@gmail.com>
2021-01-28 04:30:08 +0100

    fix service name

--- install_systemd.sh
+++ install_systemd.sh
@@ -14,18 +14,18 @@ mkdir -p "${TMP_DIR}"
 SETUP_USER="$(id -u -n)"
 SETUP_GROUP="$(id -g -n)"
 
-cp "${SCRIPT_DIR}/systemd/ton-rust-node.service" "${TMP_DIR}/ton-rust-node2.service"
-sed -i "s@ROOT@${TON_WORK_DIR}@" "${TMP_DIR}/ton-rust-node2.service"
-sed -i "s@CONFIGS@${TON_WORK_DIR}/configs@" "${TMP_DIR}/ton-rust-node2.service"
-sed -i "s@LOG@${TON_WORK_DIR}/logs/node.log@" "${TMP_DIR}/ton-rust-node2.service"
-sed -i "s@BIN@${SRC_TOP_DIR}/bin/ton_node@" "${TMP_DIR}/ton-rust-node2.service"
-sed -i "s@USER@${SETUP_USER}@" "${TMP_DIR}/ton-rust-node2.service"
-sed -i "s@GROUP@${SETUP_GROUP}@"  "${TMP_DIR}/ton-rust-node2.service"
+cp "${SCRIPT_DIR}/systemd/ton-rust-node.service" "${TMP_DIR}/ton-rust-node.service"
+sed -i "s@ROOT@${TON_WORK_DIR}@" "${TMP_DIR}/ton-rust-node.service"
+sed -i "s@CONFIGS@${TON_WORK_DIR}/configs@" "${TMP_DIR}/ton-rust-node.service"
+sed -i "s@LOG@${TON_WORK_DIR}/logs/node.log@" "${TMP_DIR}/ton-rust-node.service"
+sed -i "s@BIN@${SRC_TOP_DIR}/bin/ton_node@" "${TMP_DIR}/ton-rust-node.service"
+sed -i "s@USER@${SETUP_USER}@" "${TMP_DIR}/ton-rust-node.service"
+sed -i "s@GROUP@${SETUP_GROUP}@"  "${TMP_DIR}/ton-rust-node.service"
 
-cp "${SCRIPT_DIR}/systemd/ton-rust-node.env" "${TMP_DIR}/ton-rust-node2.env"
-sed -i "s@CONFIGS_DIR@${TON_WORK_DIR}/configs@" "${TMP_DIR}/ton-rust-node2.env"
+cp "${SCRIPT_DIR}/systemd/ton-rust-node.env" "${TMP_DIR}/ton-rust-node.env"
+sed -i "s@CONFIGS_DIR@${TON_WORK_DIR}/configs@" "${TMP_DIR}/ton-rust-node.env"
 
-sudo cp "${TMP_DIR}/ton-rust-node2.service" "${TMP_DIR}/ton-rust-node2.env" "/etc/systemd/system"
+sudo cp "${TMP_DIR}/ton-rust-node.service" "${TMP_DIR}/ton-rust-node.env" "/etc/systemd/system"
 sudo systemctl daemon-reload
 
 rm -rf "${TMP_DIR}"
